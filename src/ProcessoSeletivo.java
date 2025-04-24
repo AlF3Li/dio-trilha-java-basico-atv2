d53313f9ebb1cc916ad92f75de908f472921770c
ale <alexandref.lima3011@gmail.com>
2025-04-23 21:11:17 -0300

    Amo minha vida

---
+++ src/ProcessoSeletivo.java
@@ -0,0 +1,17 @@
+public class ProcessoSeletivo {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Processo seletivo");
+        analisarCandidato(2000.00);
+    }
+    static void analisarCandidato(double salarioPretendido){
+        double salarioBase = 2000.00;
+        if(salarioBase > salarioPretendido){
+            System.out.println("Ligar para o candidato");
+        }else if(salarioBase==salarioPretendido){
+            System.out.println("Ligar para o candidato com contra proposta");
+        }else{
+            System.out.println("Aguardando o resultado dos demais candidatos");
+        }
+    
+    }
+}
